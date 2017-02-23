6f48f01ceb4919b1ee021b3c4704d5d29617b7f2
franjpr <franpr90@gmail.com>
2017-02-23 22:21:39 +0000

    gitbook

--- scripts/deploy-gitbook.js
+++
@@ -1,8 +0,0 @@
-
-var ghp = require('gh-pages');
-var path = require('path');
-var json = require ('../package.jso');
-
-ghp.publish(path.join(__dirname, '/../txt'), { repo: json.require.url,
-												message: 'Commit from deploy-gitbook.js',
-												 }, function(param){ if(param) console.log(param);});
